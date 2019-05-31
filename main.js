994282df81d40855abe8658259424b74a809e99f
alisa17 <alisa.yunusova@gmail.com>
2019-06-01 09:06:25 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+window.onload = function() {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+context.rect(20, 20, 150, 100);
+context.stroke();
+}
